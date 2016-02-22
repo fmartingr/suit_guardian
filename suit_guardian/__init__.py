eb97facd163b475979800e9e36b83d4a4ca39fea
Felipe Martín <fmartingr@gmail.com>
2016-02-22 22:52:33 +0100

    Version bump to 038.132.0

--- suit_guardian/__init__.py
+++ suit_guardian/__init__.py
@@ -1,7 +1,7 @@
 # suit_guardian versioning:
 # <suit version updated>.<guardian version updated>.<hotfix>
 __SUIT_VERSION__ = '0.2.8'
-__GUARDIAN_VERSION__ = '1.2.0'
+__GUARDIAN_VERSION__ = '1.3.2'
 __HOTFIX__ = 0
 
 __VERSION__ = u'{}.{}.{}'.format(
